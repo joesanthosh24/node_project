a384244eb3a65ce627998ef116d82d91355afbb4
joesanthosh248 <joe.santhosh88@yahoo.com>
2020-10-27 19:53:11 -0400

    Added chat page template. Added logic on server for signUp and signIn. Created global variables in server file to hold rooms, people and error. Added some styles

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ const ejs = require("ejs");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const rooms = [
+  { name: "Room 1", people: [], messages: [] },
+  { name: "Room 2", people: [], messages: [] },
+  { name: "Room 3", people: [], messages: [] },
+];
+
+const people = [];
+let error = "";
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -14,19 +23,54 @@ app.get("/", (req, res) => {
 });
 
 app.get("/signUp", (req, res) => {
-  res.render("signUp");
+  res.render("signUp", { error });
+  error = "";
 });
 
 app.post("/signUp", (req, res) => {
-  console.log(req.body);
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const person = people.find((person) => person.email === email);
+
+  console.log(person);
+
+  if (!person) {
+    error = "";
+    people.push({ email, password });
+
+    res.redirect(`/chat?mail=${email}`);
+  }
+  else {
+    error = "User with that email already exists";
+    res.redirect("/signUp");
+  }
+});
+
+app.get("/chat", (req, res) => {
+  const email = req.query.mail;
+
+  res.render("chat", { email, rooms, people });
 });
 
 app.get("/signIn", (req, res) => {
-  res.render("signIn");
+  res.render("signIn", { error });
+  error = "";
 });
 
 app.post("/signIn", (req, res) => {
-  console.log(req.body);
+  const { email, password } = req.body;
+  const person = people.find(
+    (person) => person.email === email && person.password === password
+  );
+
+  if (person) {
+    error = "";
+    res.redirect(`/chat?mail=${email}`);
+  } else {
+    error = "Username and/or password incorrect"
+    res.redirect("/signIn")
+  }
 });
 
 app.listen(PORT, () => {
