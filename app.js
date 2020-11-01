b1fa63072c8579f073029680e83d12383b5354b9
joesanthosh248 <joe.santhosh88@yahoo.com>
2020-11-01 14:45:12 -0500

    Added functionality to add person to database when signing up and to look through people collection when signing in and look for rooms when hitting '/chat' route

--- app.js
+++ app.js
@@ -8,31 +8,35 @@ const PORT = process.env.PORT || 3000;
 
 const Schema = mongoose.Schema;
 
-mongoose.connect("localhost:27017/chatDB");
+mongoose.connect("mongodb://localhost:27017/chatDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const messageSchema = {
   message: { type: String, required: true },
-  room: { type: Schema.Types.ObjectId, ref: 'Room' }
-}
+  room: { type: Schema.Types.ObjectId, ref: "Room" },
+};
 
 const Message = mongoose.model("Message", messageSchema);
 
 const poepleSchema = {
-  name: { type: String, required: true },
-  messages: { type: [messageSchema] }
-}
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  messages: { type: [messageSchema] },
+};
 
-const People = mongoose.model("People", poepleSchema);
+const Person = mongoose.model("Person", poepleSchema);
 
 const roomSchema = {
   name: {
     type: String,
-    required: true
+    required: true,
   },
   people: {
-    type: [poepleSchema]
-  }
-}
+    type: [poepleSchema],
+  },
+};
 
 const Room = mongoose.model("Room", roomSchema);
 
@@ -56,26 +60,31 @@ app.post("/signUp", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const person = people.find((person) => person.email === email);
+  const person = new Person({
+    email,
+    password,
+    messages: [],
+  });
 
-  console.log(person);
+  Person.findOne({ email }, (err, foundPerson) => {
+    if (!foundPerson) {
+      error = "";
+      person.save();
 
-  if (!person) {
-    error = "";
-    people.push({ email, password });
-
-    res.redirect(`/chat?mail=${email}`);
-  }
-  else {
-    error = "User with that email already exists";
-    res.redirect("/signUp");
-  }
+      res.redirect(`/chat?mail=${email}`);
+    } else {
+      error = "User with that email already exists";
+      res.redirect("/signUp");
+    }
+  });
 });
 
 app.get("/chat", (req, res) => {
   const email = req.query.mail;
 
-  res.render("chat", { email, rooms, people });
+  Room.find({}, (err, rooms) => {
+    res.render("chat", { email, rooms });
+  });
 });
 
 app.get("/signIn", (req, res) => {
@@ -85,17 +94,16 @@ app.get("/signIn", (req, res) => {
 
 app.post("/signIn", (req, res) => {
   const { email, password } = req.body;
-  const person = people.find(
-    (person) => person.email === email && person.password === password
-  );
 
-  if (person) {
-    error = "";
-    res.redirect(`/chat?mail=${email}`);
-  } else {
-    error = "Username and/or password incorrect"
-    res.redirect("/signIn")
-  }
+  Person.findOne({ email }, (err, foundDocument) => {
+    if (foundDocument) {
+      error = "";
+      res.redirect(`/chat?mail=${email}`);
+    } else {
+      error = "Username and/or password incorrect";
+      res.redirect("/signIn");
+    }
+  });
 });
 
 app.listen(PORT, () => {
